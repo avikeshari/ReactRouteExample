27d2b6198ebd220c3a12fa2356f7d419d77fdf9c
avikeshari <keshari.avinash7@gmail.com>
2026-06-01 03:03:37 +0530

    Product (Recipe) Details Page Template

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Recipe = ({ recipe }) => {
-  return <li>{recipe.name}</li>
+  return <li><Link to={`/recipes/${recipe.id}`}>{recipe.name}</Link></li>
 }
 
 export default Recipe
